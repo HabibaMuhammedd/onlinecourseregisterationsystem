96a0c45567be4c39c71dafe2985ba15d8c46ae65
HabibaMuhammedd <mohamedhabiba280@gmail.com>
2026-03-16 23:28:32 +0200

    first commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int studentNUM = 2;
 int count=0;
+int courseNUM=7;
 #define num_student 50
 #define size 10
 #define num_courses 20
@@ -13,7 +14,8 @@ struct STUDENT {
     int studentPassword;
     int studentLevel;
     int registeredCOURSES[size]; // ERINY'S PART
-    int num_registeredCOURSES = 0;        // ERINY'S PART
+    int num_registeredCOURSES = 0;
+          // ERINY'S PART
 }student[num_student];
 struct COURSE{
 int ID;
@@ -29,17 +31,61 @@ struct ADMIN {
     string adminName;
   int adminPassword;
 }admin[num_admin];
+int userCHOICE();
+int signUP_OR_logIN();
+void student_initializer();
+void course_initializer();
+void admin_initializer();
+void studentLogin();
+void studentSignUp();
+void available_courses();
+int Registerd_Courses();
+void adminLogin();
+
+int main() {
+    student_initializer();
+   course_initializer();
+    admin_initializer();
+    int USERchoice = userCHOICE();
+    if (USERchoice == 1) {
+        int signORlog = signUP_OR_logIN();
+        if (signORlog == 1) {
+            studentLogin();
+            available_courses();
+            Registerd_Courses();
+        }
+        else if (signORlog == 2) {
+            studentSignUp();
+        }
+    }
+    else if (USERchoice == 2) {
+        adminLogin();
+    }
+
+    return 0;
+}
 void student_initializer() {
-  STUDENT stud[num_student]={
-    {2024567,"AYA AHMED",1234,1,7,4},
-    {2029876,"HABIBA ELSAYED",4321,2,6,3}
+
+  student[0] = {2024567,"AYA AHMED",1234,1,7,4};
+  student[1] = {2029876,"HABIBA ELSAYED",4321,2,6,3};
+
  };
-}
+void course_initializer(){
+
+    course[0] = {12,"Physics","YASMIN",300,180,"SUNDAY",3};
+    course[1] = {19,"Algorithm","OMAR",500,300,"THURSDAY",12};int ID;
+     course[2] = {5,"English","MUHAMMED",750,500,"SATURDAY",12};
+     course[3] = {14,"Artificial Intelligence","MUHAMMED",450,200,"SUNDAY",5};
+   course[4] = {11,"Cyber Security","SALSABIL",650,400,"TUESDAY",5};
+   course[5] = {13,"Linear Algebra","YARA",450,300,"MONDAY",7};
+   course[6] = {18,"Operating System","YOUSSED",450,200,"TUESDAY",5};
+
+};
 void admin_initializer() {
-ADMIN admin[num_admin]={
-  {2029345,"MAHMOUD",1111},{2345609,"AHMED",2222}
- };
+admin[0]={19,"MAHMOUD",23456};
+admin[1]={29,"AHMED",12345};
 }
+
 int userCHOICE(){
   int choice;
     const int starsNUM = 10;
@@ -55,6 +101,7 @@ int userCHOICE(){
     cout << endl;
     return choice;
 };
+
 int signUP_OR_logIN(){
  int choice;
     cout << "----- Student Portal -----" << endl;
@@ -84,7 +131,7 @@ void studentLogin(){
     }
     if (data == true) {
         cout << "Login Successful!" << endl;
-        cout << "Welcome, " << student[index].studentName << "." << endl;
+        cout << "Welcome, " << student[index].studentName << ".\n\n\n" << endl;
         // TO ERINY you should display the menu of that student if the data entered is correct
     }
     if (data == false)
@@ -111,30 +158,44 @@ void studentSignUp(){
     else
         cout << "Sorry! Maximum student capacity reached." << endl;
 };
-void available_courses(){
 
-  //if(course[num_courses].Current_enrolled< course[num_courses].Max_Capacity){
-    cout<<"Physics\n"<<"English\n"<<"ALGORITHM\n"<<"Artificial Intelligence\n"<<"Linear algebra\n"<<"Business";
-  }
-//}
+void available_courses(){
+      cout<<"Available Courses:\n\n";
+    for(int i = 0; i < courseNUM; i++){
+        cout << "Course ID: " << course[i].ID 
+             << " - " << course[i].Course_name << endl;
+    }
+};
 int Registerd_Courses(){
+    int registeredcount=0;
   int id;
   cout<<"Enter id of the course: ";
   cin>>id;
-  if(id<0||id>num_courses){
+
+  int index = -1;
+  for(int i = 0; i < num_courses; i++){
+    if(course[i].ID == id){
+      index = i;
+      break;
+    }
+  }
+  if(index == -1){
     cout<<"INVALID ID! Sorry you can't register\n";
     return 0;
   }
-    //cin>>course[num_courses].Max_Capacity;
-  if(course[id].Current_enrolled< course[id].Max_Capacity){
-  course[id].Current_enrolled++;
-   student[studentNUM].registeredCOURSES[count]=id;
- count++;
-  cout<<"Registeration Successfully ";
-  }else{
+  if(course[index].Current_enrolled < course[index].Max_Capacity){
+    course[index].Current_enrolled++;
+
+    student[studentNUM].registeredCOURSES[registeredcount] = id;
+    registeredcount++;
+
+    cout<<"Registration Successfully\n";
+  }
+  else{
     cout<<"Sorry! Course is full\n";
   }
-  return course[id].Current_enrolled ;
+
+  return course[index].Current_enrolled;
 }
 void adminLogin(){
    int id, password;
@@ -161,29 +222,6 @@ void adminLogin(){
     else if (data == false)
         cout << "Login Failed!" << endl << "Incorrect Data." << endl;
 };
-int main() {
-    student_initializer();
-    admin_initializer();
-    int USERchoice = userCHOICE();
-    if (USERchoice == 1) {
-        int signORlog = signUP_OR_logIN();
-        if (signORlog == 1) {
-            studentLogin();
-            available_courses();
-            Registerd_Courses();
-        }
-        else if (signORlog == 2) {
-            studentSignUp();
-        }
-    }
-    else if (USERchoice == 2) {
-        adminLogin();
-    }
-
-    return 0;
-}
-
-
   
 
 
